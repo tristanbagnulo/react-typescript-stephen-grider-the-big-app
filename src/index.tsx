cdfd4544f02061518857d858e1ab9308fb4beef1
tristanarup <tristan.bagnulo@arup.com>
2023-04-18 17:00:59 +1000

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import * as esbuild from 'esbuild-wasm';
+// import { start } from 'repl';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+    const ref = useRef<any>();
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        });
+    };
+
+    useEffect(() => {
+        startService();
+    }, []);
+
+    const onClick = async () => {
+        if (!ref.current) {
+            return;
+        }
+        // Handles transpiling. Is from edbuild-wasm.
+        // Remember, 'transpiling' in JS is a process that takes 
+        // any number of languages including JS ES6, TypeScript and pre
+        // ES6 JSX and transforms it into Common JS which can be executed
+        // by basically any environment e.g. an old or current browser.
+        // Below we are selecting the language to be `jsx` and the 
+        // 'target' property says the following.
+        const result = await ref.current.transform(input, {
+            loader: 'jsx',
+            target: 'es2015'
+        });
+
+        setCode(result.code); 
+    };
+
+    return <div>
+        <textarea
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+        ></textarea>
+        <div>
+            <button onClick={onClick}>Submit</button>
+        </div>
+        {/* pre elements have text that looks like code */}
+        <pre>{code}</pre>
+    </div>;
+}
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+)
